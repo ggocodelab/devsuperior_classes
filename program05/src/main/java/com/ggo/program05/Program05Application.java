c76b7fb52aa2477f545fc1e72b14c6558371a990
ggocodelab <brgillian@gmail.com>
2025-10-04 21:18:34 -0300

    program 05 first commit

---
+++ program05/src/main/java/com/ggo/program05/Program05Application.java
@@ -0,0 +1,13 @@
+package com.ggo.program05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Program05Application.class, args);
+	}
+
+}
