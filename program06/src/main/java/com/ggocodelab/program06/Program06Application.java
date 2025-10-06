bcd22b62a9167d99907290a604a43bd1dde5b380
ggocodelab <brgillian@gmail.com>
2025-10-06 11:36:51 -0300

    starter program 06

---
+++ program06/src/main/java/com/ggocodelab/program06/Program06Application.java
@@ -0,0 +1,13 @@
+package com.ggocodelab.program06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Program06Application.class, args);
+	}
+
+}
