10416efea87b5b465351be749fb3826e9c89e61a
ggocodelab <brgillian@gmail.com>
2025-10-07 15:30:56 -0300

    dscommerce first commit

---
+++ dscommercev2/src/main/java/com/ggocodelab/dscommercev2/Dscommercev2Application.java
@@ -0,0 +1,13 @@
+package com.ggocodelab.dscommercev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscommercev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscommercev2Application.class, args);
+	}
+
+}
