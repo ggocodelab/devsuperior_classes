cfb40d0ea646b852469718b41826be37dccf8d32
ggocodelab <brgillian@gmail.com>
2025-10-08 10:23:02 -0300

    Repository test

--- dscommercev2/src/main/java/com/ggocodelab/dscommercev2/controllers/ProductController.java
+++ dscommercev2/src/main/java/com/ggocodelab/dscommercev2/controllers/ProductController.java
@@ -1,15 +1,31 @@
 package com.ggocodelab.dscommercev2.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ggocodelab.dscommercev2.entities.Product;
+import com.ggocodelab.dscommercev2.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value="/products")
 public class ProductController {
 	
+	@Autowired
+	private ProductRepository repository;
+	
+	//@GetMapping
+	//private String teste() {
+		//return "Olá mundo!";
+	//}
+	
 	@GetMapping
-	private String teste() {
-		return "Olá mundo!";
-	}
+	public String teste() {
+		Optional<Product> result = repository.findById(1L);
+		Product product = result.get();
+		return product.getName();
+	}	
 }
