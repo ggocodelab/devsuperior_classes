289491c928eae45c970d23da7268e85ae6796234
ggocodelab <brgillian@gmail.com>
2025-10-08 15:45:12 -0300

    findAll pagination

--- dscommercev2/src/main/java/com/ggocodelab/dscommercev2/services/ProductService.java
+++ dscommercev2/src/main/java/com/ggocodelab/dscommercev2/services/ProductService.java
@@ -1,6 +1,10 @@
 package com.ggocodelab.dscommercev2.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,4 +24,10 @@ public class ProductService {
 				() -> new RuntimeException("Produto não encontrado"));
 		return new ProductDTO(product);		
 	}
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable){
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
+	}	
 }
