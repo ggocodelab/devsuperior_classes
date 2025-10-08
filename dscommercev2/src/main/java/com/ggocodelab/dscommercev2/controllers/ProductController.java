465ec7f47592d81f12ee8a1774dc3d6f49a51a89
ggocodelab <brgillian@gmail.com>
2025-10-08 14:42:03 -0300

    ProductController findById

--- dscommercev2/src/main/java/com/ggocodelab/dscommercev2/controllers/ProductController.java
+++ dscommercev2/src/main/java/com/ggocodelab/dscommercev2/controllers/ProductController.java
@@ -1,31 +1,24 @@
 package com.ggocodelab.dscommercev2.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ggocodelab.dscommercev2.entities.Product;
-import com.ggocodelab.dscommercev2.repositories.ProductRepository;
+import com.ggocodelab.dscommercev2.dto.ProductDTO;
+import com.ggocodelab.dscommercev2.services.ProductService;
 
 @RestController
 @RequestMapping(value="/products")
 public class ProductController {
 	
 	@Autowired
-	private ProductRepository repository;
+	private ProductService service;
 	
-	//@GetMapping
-	//private String teste() {
-		//return "Olá mundo!";
-	//}
-	
-	@GetMapping
-	public String teste() {
-		Optional<Product> result = repository.findById(1L);
-		Product product = result.get();
-		return product.getName();
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(1L);
+		return dto;
 	}	
 }
