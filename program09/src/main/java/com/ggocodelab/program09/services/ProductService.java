a018dac5ee46e714ff016b4e038526f05093db0d
ggocodelab <brgillian@gmail.com>
2025-10-10 12:43:12 -0300

    Add category name to product insert

--- program09/src/main/java/com/ggocodelab/program09/services/ProductService.java
+++ program09/src/main/java/com/ggocodelab/program09/services/ProductService.java
@@ -7,6 +7,7 @@ import com.ggocodelab.program09.dto.CategoryDTO;
 import com.ggocodelab.program09.dto.ProductDTO;
 import com.ggocodelab.program09.entities.Category;
 import com.ggocodelab.program09.entities.Product;
+import com.ggocodelab.program09.repositories.CategoryRepository;
 import com.ggocodelab.program09.repositories.ProductRepository;
 
 @Service
@@ -15,14 +16,24 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
 		entity.setName(dto.getName());
 		entity.setPrice(dto.getPrice());
 		
+        // Salva apenas o id da categoria
+//		for(CategoryDTO catDto : dto.getCategories()) {
+//			Category cat = new Category();
+//			cat.setId(catDto.getId());
+//			entity.getCategories().add(cat);
+//		}
+
+		// Salva apenas o id e nome das categorias
 		for(CategoryDTO catDto : dto.getCategories()) {
-			Category cat = new Category();
-			cat.setId(catDto.getId());
+			Category cat = categoryRepository.getReferenceById(catDto.getId()); 
 			entity.getCategories().add(cat);
 		}
 		entity = repository.save(entity);
