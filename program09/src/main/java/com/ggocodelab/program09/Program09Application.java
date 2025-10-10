38fcdd99c8dbb0efbdd36bd3b04572c52c46649d
ggocodelab <brgillian@gmail.com>
2025-10-10 10:17:32 -0300

    program09 first commit

---
+++ program09/src/main/java/com/ggocodelab/program09/Program09Application.java
@@ -0,0 +1,13 @@
+package com.ggocodelab.program09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Program09Application.class, args);
+	}
+
+}
