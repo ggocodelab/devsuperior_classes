432f1f69f62d4178560aed4790201e5f58108ee8
ggocodelab <brgillian@gmail.com>
2025-10-16 21:19:57 -0300

    contest-2738 done

--- beecrowd/beecrowd-2738/contest-2738.sql
+++ beecrowd/beecrowd-2738/contest-2738.sql
@@ -1,5 +1,4 @@
---In progress
-SELECT c.name,  ROUND(((s.match*2)+(s.specific*3)+(s.project_plan*5))/10, 2) AS "avg"
+SELECT c.name,  ROUND(((s.math*2)+(s.specific*3)+(s.project_plan*5))/10, 2) AS "avg"
 FROM candidate c
 JOIN score s ON s.candidate_id = c.id
 ORDER BY "avg" DESC;
