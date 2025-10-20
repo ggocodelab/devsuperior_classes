6aada5eaa529ff513853710512a9f7727d2ba355
ggocodelab <brgillian@gmail.com>
2025-10-20 09:36:48 -0300

    2737 v2

--- beecrowd/beecrowd-2737/lawyers-2737.sql
+++ beecrowd/beecrowd-2737/lawyers-2737.sql
@@ -1,9 +1,15 @@
 -- in progress
+-- Union All operator: https://www.w3schools.com/sql/sql_union_all.asp
+-- literal with column name: SELECT List - https://www.postgresql.org/docs/current/sql-select.html
 SELECT 
-	name,
-	CASE
-		WHEN customers_number = (SELECT MAX(customers_number) FROM lawyers) THEN customers_number
-		WHEN customers_number = (SELECT MIN(customers_number) FROM lawyers) THEN customers_number
-	END AS values
+	name,	
+	customers_number
 FROM lawyers
-GROUP BY name, customers_number;
+WHERE customers_number = (SELECT MAX(customers_number) FROM lawyers)
+   OR customers_number = (SELECT MIN(customers_number) FROM lawyers)
+UNION ALL
+SELECT
+	'Average' AS name,
+	ROUND(AVG(customers_number)) AS customers_number
+FROM lawyers
+ORDER BY name, customers_number DESC;
